c21dc01af78a3f5ee0f6fc11da821236b2f943c6
Anna <anna.xing.22@gmail.com>
2021-11-24 20:06:34 -0500

    Create function to sum transactions

---
+++ background.js
@@ -0,0 +1,7 @@
+function sumTransactions() {
+	return Array.from(document.getElementsByTagName('td'))
+        .filter(item => item.innerText.includes('$'))
+        .map(item => parseFloat(item.innerText.slice(2)))
+        .reduce((prev, curr) => prev + curr)
+        .toFixed(2);
+}
